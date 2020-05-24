09f30ed7ad82f39571520beb3c369357296b4811
Julien <juliencaron@protonmail.com>
2020-05-25 01:02:29 +0200

    Completing CRUD function for todos

--- functions/src/api/todo.ts
+++ functions/src/api/todo.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Request, Response } from 'express'
 
 import { db } from '../utils/admin'
@@ -22,10 +23,73 @@ export const getAllTodos = (req: Request, res: Response) => {
         todos.push({ id, title, body, createdAt })
       })
 
-      return res.status(200).json({ todos })
+      res.status(200).json({ todos })
     })
     .catch(err => {
       console.error(err)
-      return res.status(500).json({ error: err.code })
+      res.status(500).json({ error: err.code })
+    })
+}
+
+export const postOneTodo = (req: Request, res: Response) => {
+  if (req.body.body.trim() === '') {
+    res.status(400).json({ body: 'Must not be empty' })
+  }
+
+  if (req.body.title.trim() === '') {
+    res.status(400).json({ title: 'Must not be empty' })
+  }
+
+  const newTodoItem: Omit<Todo, 'id'> = {
+    title: req.body.title,
+    body: req.body.body,
+    createdAt: new Date(),
+  }
+
+  db.collection('todos')
+    .add(newTodoItem)
+    .then(doc => {
+      res.status(201).json({ ...newTodoItem, id: doc.id })
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ error: 'Something went wrong' })
+    })
+}
+
+export const deleteTodo = (req: Request, res: Response) => {
+  const document = db.doc(`/todos/${req.params.todoId}`)
+  document
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        res.status(404).json({ error: 'Todo not found' })
+      }
+      return document.delete()
+    })
+    .then(() => {
+      res.status(200).json({ message: 'Delete successfully' })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: err.code })
+    })
+}
+
+export const editTodo = (req: Request, res: Response) => {
+  if (req.body.todoId || req.body.createdAt) {
+    res.status(403).json({ message: 'Not allowed to edit' })
+  }
+
+  const document = db.collection('todos').doc(`${req.params.todoId}`)
+
+  document
+    .update(req.body)
+    .then(() => {
+      res.status(200).json({ message: 'Updated successfully' })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: err.code })
     })
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,13 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
 
-import { getAllTodos } from './api/todo'
+import { getAllTodos, postOneTodo, deleteTodo, editTodo } from './api/todo'
 
 const app = express()
 
 app.get('/todos', getAllTodos)
+app.post('/todo', postOneTodo)
+app.put('/todo/:todoId', editTodo)
+app.delete('/todo/:todoId', deleteTodo)
 
 export const api = functions.https.onRequest(app)
