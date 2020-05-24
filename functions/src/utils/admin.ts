391345db12b35afa590f90c627ad6624066be435
Julien <juliencaron@protonmail.com>
2020-05-25 01:02:06 +0200

    Google auth credentials as code

--- functions/src/utils/admin.ts
+++ functions/src/utils/admin.ts
@@ -1,6 +1,17 @@
 import * as admin from 'firebase-admin'
+import * as dotenv from 'dotenv'
 
-admin.initializeApp()
+dotenv.config()
+
+const credentialPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || ''
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const servicesAccount = require(credentialPath)
+
+admin.initializeApp({
+  credential: admin.credential.cert(servicesAccount) || undefined,
+  databaseURL: 'https://fir-todo-fe23e.firebaseio.com',
+})
 
 const db = admin.firestore()
 
