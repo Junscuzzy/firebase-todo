bdb5d170d608d3691fc571456d673bbdb69b3039
Julien <juliencaron@protonmail.com>
2020-06-12 00:03:16 +0200

    Fix admin auth

--- functions/src/utils/admin.ts
+++ functions/src/utils/admin.ts
@@ -1,17 +1,6 @@
 import * as admin from 'firebase-admin'
-import * as dotenv from 'dotenv'
 
-dotenv.config()
-
-const credentialPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || ''
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const servicesAccount = require(credentialPath)
-
-admin.initializeApp({
-  credential: admin.credential.cert(servicesAccount) || undefined,
-  databaseURL: 'https://fir-todo-fe23e.firebaseio.com',
-})
+admin.initializeApp()
 
 const db = admin.firestore()
 
